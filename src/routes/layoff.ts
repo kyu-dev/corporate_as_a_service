41dd13e0d62c8af9f537e0e3e08ace497ea74f55
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-28 16:07:33 +0200

    feat: route pour envoyer toutes les phrase d'un coup

--- src/routes/layoff.ts
+++ src/routes/layoff.ts
@@ -3,9 +3,16 @@ import layoffData from "../../public/layoff.json";
 
 export const layoff = new Hono();
 
+//route phrase aléatoire
 layoff.get("/", (c) => {
   const value =
     // phrases aléatoire selon la taille du tableau du json
     layoffData.phrases[Math.floor(Math.random() * layoffData.phrases.length)];
+  return c.json({ message: value });
+});
+
+//route toutes les phrases
+layoff.get("/all", (c) => {
+  const value = layoffData.phrases;
   return c.json({ message: value });
 });
