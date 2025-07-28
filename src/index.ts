f190c31210713fffd06d90f4066bca093463d6fe
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-28 17:30:58 +0200

    feat: add dontcare route to main application

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { layoff } from "./routes/layoff";
 import { corporate } from "./routes/corporate";
+import { dontcare } from "./routes/dontcare";
 
 const app = new Hono();
 
 app.route("/", corporate);
 app.route("/layoff", layoff);
+app.route("/dontcare", dontcare);
 
 export default app;
