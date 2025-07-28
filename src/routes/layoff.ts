5e95349510f62b957de16bdc0a155c817a3891b9
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-28 16:03:44 +0200

    feat : ajout de la fonctionaliter d'affichage d'une phrase random basé sur la taille du tableau contenue dans le json

--- src/routes/layoff.ts
+++ src/routes/layoff.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
+import layoffData from "../../public/layoff.json";
 
 export const layoff = new Hono();
 
 layoff.get("/", (c) => {
-  return c.text("hello you");
+  const value =
+    // phrases aléatoire selon la taille du tableau du json
+    layoffData.phrases[Math.floor(Math.random() * layoffData.phrases.length)];
+  return c.json({ message: value });
 });
